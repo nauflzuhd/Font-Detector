26638386ee459165cce903a586846d1dd0b4f20e
iyeppp <miawspeicalist@gmail.com>
2025-12-12 22:00:24 +0700

    lupa allowed origin, yang nyiptain cors pukimak

--- backend/app.py
+++ backend/app.py
@@ -29,6 +29,7 @@ else:
         "http://localhost:5000",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://font-detector.vercel.app",
     ]
 
 CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
